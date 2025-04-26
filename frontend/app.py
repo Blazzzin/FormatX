64f3f07414cc1002dc3d74e9699b7a76f25f9408
Blazzzin <145950334+Blazzzin@users.noreply.github.com>
2025-04-25 21:22:13 -0400

    Update app.py
    Update frontend flask routes with the new HTML pages added or to be added

--- frontend/app.py
+++ frontend/app.py
@@ -31,6 +31,30 @@ def signup():
 def profile():
     return render_template('profile.html')
 
+@app.route('/pdf-to-word')
+def pdf_to_word():
+    return render_template('pdf-to-word.html')
+
+@app.route('/word-to-pdf')
+def word_to_pdf():
+    return render_template('word-to-pdf.html')
+
+@app.route('/pdf-to-powerpoint')
+def pdf_to_powerpoint():
+    return render_template('pdf-to-powerpoint.html')
+
+@app.route('/powerpoint-to-pdf')
+def powerpoint_to_pdf():
+    return render_template('powerpoint-to-pdf.html')
+
+@app.route('/pdf-to-image')
+def pdf_to_image():
+    return render_template('pdf-to-image.html')
+
+@app.route('/image-to-pdf')
+def image_to_pdf():
+    return render_template('image-to-pdf.html')
+
 if __name__ == '__main__':
     webbrowser.open('http://localhost:8000')
     app.run(debug=True, port=8000, use_reloader=False)
